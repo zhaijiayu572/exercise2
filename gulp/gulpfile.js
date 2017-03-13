a5f68cb28917153c237a69a327ce8167142b38c2
zhaijiayu572 <zjypersonal@163.com>
2017-03-13 17:11:53 +0800

    gulp的使用

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -9,16 +9,17 @@ gulp.task('copy-img',function () {
     gulp.src('img/*.png').pipe(gulp.dest('dest/img'));
 });
 gulp.task('sass',function () {
-    gulp.src('*.scss').pipe(sass()).pipe(gulp.dest('dest/css'));
+    gulp.src('*.scss').pipe(sass()).pipe(gulp.dest('dest/css')).pipe(connect.reload());
 });
 gulp.task('connect',function () {
     connect.server({
         root:'dest',
         port:'8888',
-        reload:true
+        livereload:true
     })
 });
 gulp.task('watch',function () {
     gulp.watch('index.html',['task1']);
+    gulp.watch('index.scss',['sass'])
 });
 gulp.task('a',['watch','connect']);
