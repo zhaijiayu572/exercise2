9910e602c7149c8ee07ea1bc62d55b8e809e749f
zhaijiayu572 <zjypersonal@163.com>
2017-04-24 22:32:17 +0800

    webpack练习

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -1,3 +1,11 @@
-/**
- * Created by tekiyowa on 2017/4/22.
- */
+var _ = require('lodash');
+function component () {
+    var element = document.createElement('div');
+
+    /* lodash is required for the next line to work */
+    element.innerHTML = _.join(['Hello','webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,38 +1,9 @@
-/**
- * Created by tekiyowa on 2017/4/22.
- */
-var webpack = require('webpack');
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var path = require('path');
 
 module.exports = {
-    //插件项
-    plugins: [commonsPlugin],
-    //页面入口文件配置
-    entry: {
-        index : './src/js/page/index.js'
-    },
-    //入口文件输出配置
+    entry: './app/index.js',
     output: {
-        path: 'dist/js/page',
-        filename: '[name].js'
-    },
-    module: {
-        //加载器配置
-        loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.js$/, loader: 'jsx-loader?harmony' },
-            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
-        ]
-    },
-    //其它解决方案配置
-    resolve: {
-        root: 'E:/github/flux-example/src', //绝对路径
-        extensions: ['', '.js', '.json', '.scss'],
-        alias: {
-            AppStore : 'js/stores/AppStores.js',
-            ActionType : 'js/actions/ActionType.js',
-            AppAction : 'js/actions/AppAction.js'
-        }
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     }
 };
