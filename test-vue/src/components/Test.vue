a0ee841387ffba014d0832f7b0df27742b991fa9
zhaijiayu572 <zjypersonal@163.com>
2017-05-02 20:29:19 +0800

    vue-cli的练习

---
+++ test-vue/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="myTest">
+    <h1>{{ testMsg }}</h1>
+    <div>let me test the router</div>
+  </div>
+
+</template>
+<script>
+  export default{
+    name:'Test',
+    data () {
+      return {
+        testMsg: 'Hello Test'
+      }
+    }
+  }
+</script>
